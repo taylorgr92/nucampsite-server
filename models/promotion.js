b10aed65e3cdc2e39c2b914bb73ce8bd6480e420
taylorgr92 <79235815+taylorgr92@users.noreply.github.com>
2021-10-09 20:23:11 -0500

    Week 2 Workshop

---
+++ models/promotion.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+require("mongoose-currency").loadType(mongoose);
+const Currency = mongoose.Types.Currency;
+
+const partnerSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+
+    image: {
+      type: String,
+      required: true,
+    },
+    featured: {
+      type: String,
+      name: unique,
+      required: false,
+    },
+    cost: {
+      type: Currency,
+      required: true,
+      min: 0,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    partner: [partnerSchema],
+  },
+  {
+    timestamps: true,
+  }
+);
+const Partner = mongoose.model("Partner", partnerSchema);
+
+module.exports = Partner;
--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,81 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+connect.then(() => {
+
+  console.log('Connected correctly to server');
+
+  Campsite.create({
+      name: 'React Lake Campground',
+      description: 'test'
+  })
+  .then(campsite => {
+      console.log(campsite);
+      return Campsite.find();
+  })
+  .then(campsites => {
+      console.log(campsites);
+      return Campsite.deleteMany();
+  })
+  .then(() => {
+      return mongoose.connection.close();
+  })
+  .catch(err => {
+      console.log(err);
+      mongoose.connection.close();
+  });
+});
+
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+const connect = mongoose.connect(url, {
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+connect.then(() => {
+
+  console.log('Connected correctly to server');
+
+  Campsite.create({
+      name: 'React Lake Campground',
+      description: 'test'
+  })
+  .then(campsite => {
+      console.log(campsite);
+
+      return Campsite.findByIdAndUpdate(campsite._id, {
+          $set: { description: 'Updated Test Document' }
+      }, {
+          new: true
+      });
+  })
+  .then(campsite => {
+      console.log(campsite);
+
+      campsite.comments.push({
+          rating: 5,
+          text: 'What a magnificent view!',
+          author: 'Tinus Lorvaldes'
+      });
+
+      return campsite.save();
+  })
+  .then(campsite => {
+      console.log(campsite);
+      return Campsite.deleteMany();
+  })
+  .then(() => {
+      return mongoose.connection.close();
+  })
+  .catch(err => {
+      console.log(err);
+      mongoose.connection.close();
+  });
 });
 
 module.exports = router;
